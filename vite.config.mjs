5869e4e90a32139bc8e27517e8d7b965131ea20a
Dario Chavez <dc6175370@gmail.com>
2025-05-08 17:06:33 -0600

    tralalero tralala

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
-import Inspect from 'vite-plugin-inspect';
-
 
 export default defineConfig({
-  plugins: [vue(), Inspect()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

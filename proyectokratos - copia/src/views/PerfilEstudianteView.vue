e0ce66c023abfcd0d4c54817fcda1f4a718b20b6
pablo <pablobecer0@gmail.com>
2025-05-08 17:04:21 -0600

    actializacion del vin diesel

---
+++ proyectokratos - copia/src/views/PerfilEstudianteView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1></h1>
+      <PerfilEstudiante />
+    </div>
+  </template>
+  
+  <script>
+  import PerfilEstudiante from '../components/PerfilEstudiante.vue'
+  
+  export default {
+    components: { PerfilEstudiante }
+  }
+  </script>
+  
